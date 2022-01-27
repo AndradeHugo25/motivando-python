3f9843455a46f5c4ad445959794e732a84f7e142
AndradeHugo <andradehugo@id.uff.br>
2022-01-27 20:44:52 -0300

    git ignore

--- b_condicionais/Condicionais.py
+++ b_condicionais/Condicionais.py
@@ -1,1 +1,15 @@
-
+# x = 1
+# y = 2
+#
+# if 2 <= x <= 100:
+#     print('X está dentro do intervalo')
+# else:
+#     print('X está fora do intervalo')
+#
+# if 2 <= y <= 100:
+#     print('Y está dentro do intervalo')
+# else:
+#     print('Y está fora do intervalo')
+#
+#
+#
