e964e82b2917e90e8a47c2b270a51a548c499274
AndradeHugo <andradehugo@id.uff.br>
2022-02-03 18:38:39 -0300

    repetição

---
+++ c_repeticao/EstruturasRepeticao.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =========================CONTINUE==========================
+# for num in range(5):
+#     if num == 3:
+#         print("Encontrei o 3")
+#         # Executa o continue, pulando para o próximo laço
+#         continue
+#     else:
+#         print(num)
+#     # Não executado quando num == 3
+#     print("Estou abaixo do IF")
+
+# =========================ENUMERATE========================
+# computador = ['Processador', 'Teclado', 'Mouse']
+# for i, valor in enumerate(computador):
+#     print(f"Índice={i} | Valor={valor}")
+#
+# computador = ['Processador', 'Teclado', 'Mouse']
+#
+# tamanho = len(computador)
+# print(f"tamanho da lista {tamanho}")
+#
+# for i in range(tamanho):
+#     print(f"Índice={i} | Valor={computador[i]}")
